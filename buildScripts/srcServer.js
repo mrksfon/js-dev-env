c18cb5f1254749aee1f556daef00fd6630a8ce34
Marko <markostevic96@hotmail.com>
2017-11-18 03:35:04 +0100

    babel

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
 
-var port = 3001;
-var app = express();
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const port = 3001;
+const app = express();
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
